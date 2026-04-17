b44f9cb85bcda0c7a1fb452ee093ec03cac749e0
voyagerdev26 <devs251898@gmail.com>
2026-04-17 19:24:48 +0530

    version 3

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 
-const LoginForm=({setIsLoggedIn})=>{
+let LoginForm=({setIsLoggedIn})=>{
+  let [name,setName]= useState("");
+  let [pass,setPass]=useState("");
   return (
-    <form onSubmit={(e)=>{e.preventDefault();setIsLoggedIn(true);}}>
+    <form onSubmit={(e)=>{e.preventDefault();
+      if(name && pass){
+
+        setIsLoggedIn(true);
+      }
+    
+    }}>
       <label>Username:</label>
-      <input type="text"  />
+      <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
       <label>Password:</label>
-      <input type="text"  />
+      <input type="text" value={pass} onChange={(e)=>{setPass(e.target.value)}} />
       <br>
       <button >Login</button>
     </form>
